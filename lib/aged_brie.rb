9e89a01547e53b342c5ed4a2ef97d52e9aefe35b
becc-mu <becc.mulugeta@gmail.com>
2018-12-15 21:19:10 +0000

    updated update quality and max quality method

--- lib/aged_brie.rb
+++ lib/aged_brie.rb
@@ -1,17 +1,30 @@
 class AgedBrie
-  attr_reader :name, :sell_in, :quality
+ attr_reader :name, :sell_in, :quality
  def initialize(name, sell_in, quality)
    @name = name
    @quality = quality
    @sell_in = sell_in
  end
 
+ def to_s
+   "#{@name}, #{@sell_in}, #{@quality}"
+ end
+
  def update
    @quality += 1 if @quality == 49
+   unless max_quality?
+     @quality += 2 if sell_in.zero?
+     @quality += 1 unless sell_in.zero?
+   end
+   reduce_sell_in
  end
 
  def reduce_sell_in
    @sell_in -= 1
+ end
+
+ def max_quality?
+   @quality == 50
  end
 
 end
