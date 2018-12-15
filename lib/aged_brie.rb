b136c4c96ecd82eb229d3cea693472c41c7e7a4a
becc-mu <becc.mulugeta@gmail.com>
2018-12-15 20:56:09 +0000

    initialized Aged brie class and added update, sell_in  methods

--- lib/aged_brie.rb
+++ lib/aged_brie.rb
@@ -1,2 +1,17 @@
 class AgedBrie
-end 
+  attr_reader :name, :sell_in, :quality
+ def initialize(name, sell_in, quality)
+   @name = name
+   @quality = quality
+   @sell_in = sell_in
+ end
+
+ def update
+   @quality += 1 if @quality == 49
+ end
+
+ def reduce_sell_in
+   @sell_in -= 1
+ end
+
+end
