1aef7aa2a21a66727ea9fd3e4612960d71ec7472
becc-mu <becc.mulugeta@gmail.com>
2018-12-15 20:41:53 +0000

    added more aged brie test

--- spec/aged_brie_spec.rb
+++ spec/aged_brie_spec.rb
@@ -1,4 +1,5 @@
 require 'aged_brie'
+
 describe AgedBrie do
   describe '#updates' do
     context 'before sell_in' do
@@ -26,6 +27,35 @@ describe AgedBrie do
         item = AgedBrie.new("Aged Brie", 0, 49)
          GildedRose.new([item]).update_quality
         expect(item.quality).to eq(50)
+      end
+    end
+    context 'when item is Aged Brie' do
+      context 'before sell_in' do
+        it 'raises quality by one after a day' do
+          item = AgedBrie.new("Aged Brie", 1, 0)
+          GildedRose.new([item]).update_quality
+          expect(item.quality).to eq(1)
+        end
+
+        it 'never raises quality beyond 50' do
+          item = AgedBrie.new("Aged Brie", 1, 50)
+          GildedRose.new([item]).update_quality
+          expect(item.quality).to eq(50)
+        end
+      end
+
+      context 'after sell_in' do
+        it 'raises quality by two after a day' do
+          item = AgedBrie.new("Aged Brie", 0, 0)
+          GildedRose.new([item]).update_quality
+          expect(item.quality).to eq(2)
+        end
+
+        it 'only raises quality to 50 when quality is at 49' do
+          item = AgedBrie.new("Aged Brie", 0, 49)
+          GildedRose.new([item]).update_quality
+          expect(item.quality).to eq(50)
+        end
       end
     end
   end
