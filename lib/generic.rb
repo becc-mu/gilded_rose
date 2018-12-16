4cb200cdbe57bb0789eb4d06680c6a1a153ffa77
becc-mu <becc.mulugeta@gmail.com>
2018-12-16 00:38:51 +0000

    update generic class

--- lib/generic.rb
+++ lib/generic.rb
@@ -1,2 +1,15 @@
 class Generic
-end 
+  attr_reader :name, :sell_in, :quality
+  def initialize(name, sell_in, quality)
+    @name = name
+    @quality = quality
+    @sell_in = sell_in
+  end
+
+  def to_s
+    "#{@name}, #{@sell_in}, #{@quality}"
+  end
+
+  def update
+  end 
+end
